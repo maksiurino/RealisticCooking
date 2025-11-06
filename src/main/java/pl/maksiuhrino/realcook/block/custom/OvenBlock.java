82e667f453d2830ef91034873807c619abc79054
maksiurino <153639807+maksiurino@users.noreply.github.com>
2025-11-06 09:47:02 +0100

    Added

--- src/main/java/pl/maksiuhrino/realcook/block/custom/OvenBlock.java
+++ src/main/java/pl/maksiuhrino/realcook/block/custom/OvenBlock.java
@@ -2,11 +2,14 @@ package pl.maksiuhrino.realcook.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.*;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.Direction;
 import pl.maksiuhrino.realcook.block.custom.vanilla.TransparentHorizontalFacingBlock;
 
 public class OvenBlock extends TransparentHorizontalFacingBlock {
     public static final MapCodec<OvenBlock> CODEC = createCodec(OvenBlock::new);
+    public static final BooleanProperty OPEN = BooleanProperty.of("open");
 
     @Override
     protected MapCodec<? extends OvenBlock> getCodec() {
@@ -16,5 +19,10 @@ public class OvenBlock extends TransparentHorizontalFacingBlock {
     public OvenBlock(Settings settings) {
         super(settings);
         this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING, OPEN);
     }
 }
